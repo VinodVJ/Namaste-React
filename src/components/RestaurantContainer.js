1e1e3cfaa11c9183c72fdb59e0f7bffb5024aad0
Vinod Jayaram <vinodj.2194@gmail.com>
2024-10-30 16:35:59 +0530

    Episode-05

--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -1,13 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
 import resData from "../utils/mockData";
+import { useState } from "react";
 
 const RestaurantContainer = () => {
+    const [restaurantList, setRestaurantList] = useState(resData);
+
     return (
-        <div className='res-container'>
-            {
-                resData.map((resturant) =>
-                    <RestaurantCard key={resturant.info.id} resData={resturant} />)
-            }
+        <div>
+            <div className="res-btn">
+                <button onClick={() => {
+                    const filteredList = resData.filter((res) => res?.info?.avgRating > 4.2)
+                    console.log(filteredList);
+                    setRestaurantList(filteredList);
+                }}>Top Rated Resturants</button>
+            </div>
+            <div className='res-container'>
+                {
+                    restaurantList.map((resturant) =>
+                        <RestaurantCard key={resturant.info.id} resData={resturant} />)
+                }
+            </div>
         </div>
     );
 }
